fe60b654d9067c80ef96c45714debdf2a06ce7b7
Dmitriy Budakov <60847626+dbudakov@users.noreply.github.com>
2021-10-18 10:57:19 +0300

    Update docker_install.sh

--- 11.Docker/docker_install.sh
+++ 11.Docker/docker_install.sh
@@ -1,5 +1,5 @@
 sudo yum check-update
-curl -fsSL https://get.docker.com/ | sed 's/sleep 20/sleep 2/' | sh
+curl -fsSL https://get.docker.com/ | sed 's/sleep 20/sleep 2/g' | sh
 sudo usermod -aG docker root 
 sudo systemctl start docker
 sudo systemctl enable docker
